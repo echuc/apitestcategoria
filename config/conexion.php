9da0052fd4a9e60be866bec5fa290acca9f0ecd1
Esdras Chuc <esdraschuc@MacBook-Pro-de-Esdras.local>
2022-07-05 22:55:36 -0500

    05072022 Se actualizo el link de conexión de BD de heroku

--- config/conexion.php
+++ config/conexion.php
@@ -4,7 +4,9 @@
         // función para conectarse a la BD
         protected function Conexion() {
             try {
-                $conectar = $this->bdhost = new PDO("mysql:host=localhost;dbname=bd_apiwservice","root","");
+                // $conectar = $this->bdhost = new PDO("mysql:host=localhost;dbname=bd_apiwservice","root","");
+                // Cadena de conexión
+                $conectar = $this->bdhost = new PDO("mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_6a0362840d6f267","b8bb15def1d288","580b71ff");
                 return $conectar;
             } catch (Exception $e) {
                 print "!Error : ". $e->getMessage() . "<br>";
